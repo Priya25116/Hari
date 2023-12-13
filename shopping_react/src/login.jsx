006bcef097f80723f9a2eeb8a49a358f4120bd38
Priya25116 <134248222+Priya25116@users.noreply.github.com>
2023-12-13 23:14:52 +0530

    commit

---
+++ shopping_react/src/login.jsx
@@ -0,0 +1,15 @@
+export function Login()
+{
+    return(
+        <div>
+            <h2>Login</h2>
+            <dl>
+                <dt>User Name</dt>
+                <dd><input type="text"></input></dd>
+                <dt>Password</dt>
+                <dd><input type="password"></input></dd>
+            </dl>
+            <button>Login</button>
+        </div>
+    )
+}
